f21725527765a5253c7eee1a19d098f9b111841d
ag563 <ag563@users.noreply.github.com>
2021-02-07 15:58:11 -0500

    Improved rafactoring for reuse and testing

--- dosing.py
+++ dosing.py
@@ -6,7 +6,8 @@
 """
 
 
-def dose_amount():
+    # INPUT
+def get_input():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -19,6 +20,10 @@ def dose_amount():
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
     weight_input = input("Enter weight: ")
+    return weight_input, diagnosis
+
+    ## DATA MANIPULATION
+def data_manipulation(weight_input, diagnosis):
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
     units = weight_data[1]
@@ -27,12 +32,21 @@ def dose_amount():
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
+    return dosage_mg_first_day, weight
+
+    ## OUTPUT
+def do_output(dosage_mg_first_day, weight):
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
           .format(dosage_mg_first_day))
 
+def dosing_function():
+    weight, diag = get_input()
+    dosage_amount, weight = data_manipulation(weight, diag)
+    do_output(dosage_amount, weight)
+
 
 if __name__ == '__main__':
-    dose_amount()
+    dosing_function()
 
